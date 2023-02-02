ac8458159cb58f1072638d345dc06a47d82a356d
Federico Urbani <urbanifederico5@gmail.com>
2023-02-02 21:30:53 +0100

    updated 0202

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car {
+
+String modelName;
+String mileage;
+
+    public Car(String modelName, String mileage) {
+        this.modelName = modelName;
+        this.mileage = mileage;
+    }
+
+    @Deprecated
+    public void printCarDetails(){
+
+        System.out.print(this.modelName+" "+this.mileage);
+
+    }
+    public void getCarDetails(){
+        System.out.println(this.modelName);
+        System.out.println(this.mileage);
+
+
+    }
+}
---
+++ Tester.java
@@ -0,0 +1,11 @@
+public class Tester {
+
+    public static void main(String[] args) {
+        Car car1= new Car("Nissan","90000");
+
+        car1.printCarDetails();
+        System.out.println();
+        car1.getCarDetails();
+    }
+
+}
